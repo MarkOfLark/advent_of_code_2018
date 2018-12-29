0ab2b3f727ef822535c48e1a71fb20a8a387ae1f
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2018-12-29 11:47:24 -0500

    more work on day 6

--- aoc2018_day06/src/lib.rs
+++ aoc2018_day06/src/lib.rs
@@ -7,10 +7,7 @@ struct Coordinate {
 
 impl Coordinate {
     fn manhattan_distance_to(&self, other: &Coordinate) -> u32 {
-        self.manhattan_distance_to(other.x,other.y)
-    }
-    fn manhattan_distance_to(&self, other_x: i32, other_y: i32) -> u32 {
-        ((self.x - other_x).abs() + (self.y - other_y).abs()) as u32
+        ((self.x - other.x).abs() + (self.y - other.y).abs()) as u32
     }
 }
 
@@ -28,6 +25,37 @@ fn parse_coordinates(input: &str) -> Vec<Coordinate> {
     return c;
 }
 
+
+
+fn find_nearest_coord(x: i32, y: i32, coords: &[Coordinate]) -> Option<usize> {
+    let mut nearest_measurement = std::u32::MAX;
+    let mut nearest_coord_idx = 0usize;
+    let mut other_nearest_coord_idx = 0usize;
+
+    for c in coords.iter() {
+        let dist = c.manhattan_distance_to(&Coordinate{idx:0usize,x:x,y:y});
+                
+        if dist <= nearest_measurement {
+            nearest_measurement = dist;
+            nearest_coord_idx = c.idx;
+
+            if dist < nearest_measurement {
+                other_nearest_coord_idx = c.idx;
+            }
+        }
+    }
+
+    if other_nearest_coord_idx == nearest_coord_idx {
+        Some(nearest_coord_idx)
+    }
+    else {
+        None
+    }
+}
+
+
+
+
 #[no_mangle]
 pub fn aoc_solution(star: i32, input: &str) -> String {
 
@@ -35,14 +63,20 @@ pub fn aoc_solution(star: i32, input: &str) -> String {
     let max_y = coords.iter().map(|c| c.y).max().unwrap();
     let max_x = coords.iter().map(|c| c.x).max().unwrap();
 
-    let area = vec![0u32; coords.len()];
+    let mut area = vec![0u32; coords.len()];
     let is_inf = vec![false; coords.len()];
 
+    // Count the areas
     for x in 0..=max_x {
         for y in 0..=max_y {
-
+            if let Some(idx) = find_nearest_coord(x,y,&coords) {
+                area[idx] += 1;
+            }
         }
     }
+    println!("{:?}",area);
+
+    // Loop over the edges again
 
     "not implemented".to_string()
 }
