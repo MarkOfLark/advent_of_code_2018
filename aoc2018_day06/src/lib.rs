4957698ebbfe13a86508b2ddd43f56bb15eae417
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2018-12-31 15:03:59 -0500

    Finish day 6 (both stars)

--- aoc2018_day06/src/lib.rs
+++ aoc2018_day06/src/lib.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::iter::FromIterator;
+
+
 #[derive(Debug)]
 struct Coordinate {
     idx: usize,
@@ -34,16 +38,21 @@ fn find_nearest_coord(x: i32, y: i32, coords: &[Coordinate]) -> Option<usize> {
 
     for c in coords.iter() {
         let dist = c.manhattan_distance_to(&Coordinate{idx:0usize,x:x,y:y});
-                
-        if dist <= nearest_measurement {
-            nearest_measurement = dist;
-            nearest_coord_idx = c.idx;
 
+        //println!("  {}: ({},{}) to ({},{}) is {}",c.idx,c.x,c.y,x,y,dist);
+        if dist <= nearest_measurement {
+            //println!("  {}: nearest_measurement was {}, now it is {}",c.idx,nearest_measurement,dist);
             if dist < nearest_measurement {
                 other_nearest_coord_idx = c.idx;
             }
+
+            nearest_measurement = dist;
+            nearest_coord_idx = c.idx;
+
         }
     }
+
+    //println!("  idx {}, other idx {}",nearest_coord_idx,other_nearest_coord_idx);
 
     if other_nearest_coord_idx == nearest_coord_idx {
         Some(nearest_coord_idx)
@@ -51,6 +60,14 @@ fn find_nearest_coord(x: i32, y: i32, coords: &[Coordinate]) -> Option<usize> {
     else {
         None
     }
+}
+
+fn sum_distances(x: i32, y: i32, coords: &[Coordinate]) -> u32 {
+    let mut dist = 0u32;
+    for c in coords.iter() {
+        dist += c.manhattan_distance_to(&Coordinate{idx:0usize,x:x,y:y});
+    }
+    dist
 }
 
 
@@ -63,20 +80,50 @@ pub fn aoc_solution(star: i32, input: &str) -> String {
     let max_y = coords.iter().map(|c| c.y).max().unwrap();
     let max_x = coords.iter().map(|c| c.x).max().unwrap();
 
-    let mut area = vec![0u32; coords.len()];
-    let is_inf = vec![false; coords.len()];
-
-    // Count the areas
-    for x in 0..=max_x {
-        for y in 0..=max_y {
-            if let Some(idx) = find_nearest_coord(x,y,&coords) {
-                area[idx] += 1;
+    if 1 == star {
+        let mut area = HashMap::new();
+        for i in 0..coords.len() {
+            area.insert(i,0usize);
+        }
+    
+        // Count the areas
+        for x in 0..=max_x {
+            for y in 0..=max_y {
+                if let Some(idx) = find_nearest_coord(x,y,&coords) {
+                    *area.get_mut(&idx).unwrap() += 1;
+                }
             }
         }
+
+        // Loop over the edges to remove any infinite regions
+        for x in 0..=max_x {
+            if let Some(idx) = find_nearest_coord(x,0,&coords) {
+                area.remove(&idx);
+            }
+            if let Some(idx) = find_nearest_coord(x,max_y,&coords) {
+                area.remove(&idx);
+            }
+        }
+        for y in 0..=max_y {
+            if let Some(idx) = find_nearest_coord(0,y,&coords) {
+                area.remove(&idx);
+            }
+            if let Some(idx) = find_nearest_coord(max_x,y,&coords) {
+                area.remove(&idx);
+            }
+        }
+        format!("{}",area.values().max().unwrap())
     }
-    println!("{:?}",area);
+    else {
+        let mut area = 0u32;
 
-    // Loop over the edges again
-
-    "not implemented".to_string()
+        for x in 0..=max_x {
+            for y in 0..=max_y {
+                if 10000 > sum_distances(x,y,&coords) {
+                    area += 1;
+                }
+            }
+        }
+        area.to_string()
+    }
 }
